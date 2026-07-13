8294117e755a915220062d78eab6e31ebe01ab89
Debasish-Sahoo1124 <debasishsahoo871@gmail.com>
2026-07-13 10:33:55 +0530

    Time: 0 ms (100%), Space: 23.9 MB (56.17%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n=nums.size();
+        int i=0;
+        while(i<n and nums[i]!=0){
+            i++;
+        }
+        int j=i+1;
+        while(j<n){
+            if(nums[j]!=0 ) {
+                swap(nums[i],nums[j]);
+                i++;
+                j++;
+            }
+            else{
+                j++;
+            }
+        }
+    }
+};
