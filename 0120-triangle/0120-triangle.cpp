ca9ba550944c7d3f397274727bffa4c8ccc1d136
Debasish-Sahoo1124 <debasishsahoo871@gmail.com>
2026-07-24 10:41:13 +0530

    Time: 0 ms (100%), Space: 12.8 MB (70.22%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -9,9 +9,8 @@ public:
         for(int i=n-2;i>=0;i--){
             vector<int>curr(n,0);
             for(int j=i;j>=0;j--){
-              int bottom=triangle[i][j]+prev[j];
-              int diagonal=triangle[i][j]+prev[j+1];
-              curr[j]=min(bottom,diagonal);
+              
+              curr[j]= triangle[i][j]+min(prev[j],prev[j+1]);
             }
             prev=curr;
         }
